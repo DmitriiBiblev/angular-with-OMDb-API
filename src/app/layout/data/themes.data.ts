3a430f22bbb6ec305399533ffe7d207c850951a8
Dmitrii Biblev <39480540+DmitriiBiblev@users.noreply.github.com>
2023-01-19 22:49:29 +0300

    Feature/create layout (#2)
    * created header and main styles
    
    * created click-outside directive
    
    * enhancement style for icons
    
    * added github link for header
    
    * added default color for icons
    
    * enhancement header and theme-button
    
    * enhancement global styles
    
    * changed favicon

---
+++ src/app/layout/data/themes.data.ts
@@ -0,0 +1,7 @@
+import { ITheme } from '../interfaces';
+
+export const THEMES_DATA: ITheme[] = [
+  { type: 'default', text: 'OS Default' },
+  { type: 'light', text: 'Light' },
+  { type: 'dark', text: 'Dark' },
+];
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
-import { HeaderComponent, LayoutComponent } from './components';
+import { HeaderComponent, LayoutComponent, ThemeButtonComponent } from './components';
+import { ClickOutsideDirective } from './directives';
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
     LayoutComponent,
+    ThemeButtonComponent,
+    ClickOutsideDirective,
   ],
   imports: [
     CommonModule,
